f17f4d7b248fc94859756d8f83391b5d24f78e9b
parksys <qkrwns285@gmail.com>
2024-05-28 15:23:15 +0900

    work-2

---
+++ src/test/java/org/example/AppTests.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTests {
+    @Test
+    void t1(){
+        assertThat(new App().plus(a:10,b:20)).isEqualTo(30);
+    }
+
+
+}
